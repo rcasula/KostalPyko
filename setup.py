1c51781823f890830b680c8b0cc723e65c6d6392
Giel <gieljnssns@me.com>
2020-07-12 11:58:11 +0200

    update version

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='kostalpyko',
-    version='v0.4',
-    packages=['tests', 'kostalpyko'],
-    install_requires=[
-          'lxml',
-      ],
-    url='https://github.com/gieljnssns/KostalPikoPy',
-    license='MIT',
-    author='Giel Janssens',
-    author_email='gieljnssns@vivaldi.net',
-    description='A package for working with a Piko Inverter from Kostal'
+    name="kostalpyko",
+    version="v0.5",
+    packages=["tests", "kostalpyko"],
+    install_requires=["lxml",],
+    url="https://github.com/gieljnssns/KostalPikoPy",
+    license="MIT",
+    author="Giel Janssens",
+    author_email="gieljnssns@vivaldi.net",
+    description="A package for working with a Piko Inverter from Kostal",
 )
