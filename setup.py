01c29530bc053011365b295f167523aaf196fd78
Roberto Casula <roberto.casula@injenia.it>
2021-11-29 11:53:36 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="kostalpiko",
-    version="v0.2",
+    version="v0.3",
     packages=["tests", "kostalpiko"],
     install_requires=["lxml",],
     url="https://github.com/rcasula/kostalpiko",
